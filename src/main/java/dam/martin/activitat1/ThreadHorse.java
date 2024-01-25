ae9a81bc41c6c2e97516510979fd0a6cbba21456
Martin Peidro <peidromartin@gmail.com>
2024-01-25 08:55:04 +0100

    First

---
+++ src/main/java/dam/martin/activitat1/ThreadHorse.java
@@ -0,0 +1,30 @@
+package dam.martin.activitat1;
+
+public class ThreadHorse implements Runnable{
+    String nombreCaballo;
+
+    public ThreadHorse(String nombreCaballo) {
+        this.nombreCaballo = nombreCaballo;
+    }
+
+    @Override
+    public void run() {
+        int paso = 100;
+        int distanciaTotal = 5000;
+
+        while (distanciaTotal > 0){
+            try {
+                Thread.sleep(200);
+                distanciaTotal -= paso;
+
+                if (distanciaTotal > 0) {
+                    System.out.println(nombreCaballo + ": " + distanciaTotal + "metros para finalizar");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println( nombreCaballo + ": Ha finalizado la carrera");
+    }
+}
